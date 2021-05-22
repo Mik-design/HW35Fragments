600a8f319a201a7ab6569f472ae32c493ed8ec23
Mik-design <77226569+Mik-design@users.noreply.github.com>
2021-05-22 16:27:55 +0600

    Add files via upload

---
+++ Fragment1.kt
@@ -0,0 +1,18 @@
+package kg.tutorial.hw35fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment1: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment1, container, false)
+        return rootView
+    }
+}
